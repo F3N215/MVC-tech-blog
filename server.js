3d9287e40fd6e388403982e943c32af7c6808f79
Daniel John <dcampo85@gmail.com>
2024-03-02 15:11:07 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+// import modules
+const path = require("path");
+const express = require("express");
+
+// handlebars import syntax
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create();
+
+const routes = require("./controllers"); // import routes
