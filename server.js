9792e75a3a91306ddf0f67f6882d1c8ff77b87b9
Daniel John <dcampo85@gmail.com>
2024-03-02 15:30:05 -0500

    middleware add-ins

--- server.js
+++ server.js
@@ -7,3 +7,14 @@ const exphbs = require("express-handlebars");
 const hbs = exphbs.create();
 
 const routes = require("./controllers"); // import routes
+const sequelize = require("./config/connect.js"); // import sequelize instance
+const app = express();
+const PORT = 3001;
+
+app.engine("handlebars", hbs.engine); // given template registers as handlebars
+app.set("view engine", "handlebars");
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ exended: true }));
+app.use(express.static(path.join(__dirname, "public"))); //static files will be in the public folder, this joins the root path with it
