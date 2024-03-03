3c9bdb05dface4dc807988d706a31541913d4bdb
Daniel John <dcampo85@gmail.com>
2024-03-02 20:08:34 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 // import modules
 const path = require("path");
-const express = require("express");
-
-// handlebars import syntax
+const express = require("express"); // handlebars import syntax
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create();
-
 const routes = require("./controllers"); // import routes
-const sequelize = require("./config/connect.js"); // import sequelize instance
+const sequelize = require("./config/connection.js"); // import sequelize instance
+
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create();
 
 app.engine("handlebars", hbs.engine); // given template registers as handlebars
 app.set("view engine", "handlebars");
@@ -18,7 +17,9 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ exended: true }));
 app.use(express.static(path.join(__dirname, "public"))); //static files will be in the public folder, this joins the root path with it
+
 app.use(routes);
+
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Server is now running at port ${PORT}`));
+  app.listen(PORT, () => console.log("Now listening"));
 });
