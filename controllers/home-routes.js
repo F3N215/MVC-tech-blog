cc499122621c0143c9f605e56fbc3fd13eb76c97
Daniel John <dcampo85@gmail.com>
2024-03-03 17:43:29 -0500

    update homeroutes with catch

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,27 +1,36 @@
-const router = require('express').Router();
-const { Post, User, Comment } = require('../mdoels');
-const sequelize = require('../congi/config');
+const router = require("express").Router();
+const { Post, User, Comment } = require("../mdoels");
+const sequelize = require("../congi/config");
 
-router.get('/', async (req, res) => {
-    try {
-        const dbPostData = await Post.findAll({
-            attributes: ['id', 'title', 'content', 'created_at'],
-            include: [
-                {
-                    model: Comment,
-                    attributes; ['id', 'comment', 'postId', 'userId', 'created_at'],
-                    include: {
-                        model: User,
-                        attributes: ['username'],
-                    },
-                },
-                {
-                    model: User,
-                    attributes: ['username'],
-                },
-            ],
-            order: [['created_at', 'DESC']],
-        })
-        const post = db
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      attributes: ["id", "title", "content", "created_at"],
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "comment", "postId", "userId", "created_at"],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+      order: [["created_at", "DESC"]],
+    });
+    const post = dbPostData.map((post) => post.get({ plain: true }));
+    console.log(posts);
+    res.render("homepage", {
+      posts,
+      loggedIn: req.session.loggedIn,
+      username: req.session.username,
+      userId: req.session.userId,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
