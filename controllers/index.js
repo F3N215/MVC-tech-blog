353ce189be94a583c1773c49be20119e29eb8e69
Daniel John <dcampo85@gmail.com>
2024-03-03 16:38:04 -0500

    connection.js updates

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@
 const router = require("express").Router();
 const User = require("../models");
 
-router.get("/signup", async (req, res) => {
+router.get("/", async (req, res) => {
   res.render("signup");
 });
 
