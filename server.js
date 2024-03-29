4de60c20c47de04a7928519f38578e2f9ef75214
Daniel John <dcampo85@gmail.com>
2024-03-28 22:37:55 -0400

    updates to server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const routes = require("./controllers");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/config");
+const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
