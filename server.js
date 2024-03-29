e35ff4c26e6737e220a14c8b241ccf4da9ec592a
Daniel John <dcampo85@gmail.com>
2024-03-28 22:36:41 -0400

    updates to server.js

--- server.js
+++ server.js
@@ -9,18 +9,36 @@ const routes = require("./controllers");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create();
+const sequelize = require("./config/config");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-app.engine("handlebars", hbs.engine); // given template registers as handlebars
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 300000,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-// middlewares
 app.use(express.json());
-app.use(express.urlencoded({ exended: true }));
-app.use(express.static(path.join(__dirname, "public"))); //static files will be in the public folder, this joins the root path with it
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
 });
