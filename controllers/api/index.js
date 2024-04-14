0923b993120a8e827a0b58701c0743c25ec5ff89
Daniel John <dcampo85@gmail.com>
2024-04-14 13:06:54 -0400

    fix routers

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,15 @@ const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
 const postRoutes = require("./post-routes");
+const apiRoutes = require("./api");
+const dashboardRoutes = require("./dashboard-routes");
 const commentRoutes = require("./comment-routes");
 
 // all of these routes are prefixed with '/api'
-router.use("/user", userRoutes);
+router.use("/", userRoutes);
 router.use("/post", postRoutes);
 router.use("/comment", commentRoutes);
+router.use("/dashboard", dashboardRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
