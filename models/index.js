26bf932c209cf5d2ad9b275374f927638efe0529
Daniel John <dcampo85@gmail.com>
2024-04-14 13:54:28 -0400

    fix index.js models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,34 @@
 // imports model definition from respective model files in the folder
-const User = require("./user");
-module.exports = { User };
+const User = require("./User");
+const Comment = require("./Comment");
+const Post = require("./Post");
+
+User.hasMany(Post, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "userId",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "postId",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "userId",
+  onDelete: "CASCADE",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "postId",
+  onDelete: "CASCADE",
+});
+
+module.exports = { User, Comment, Post };
